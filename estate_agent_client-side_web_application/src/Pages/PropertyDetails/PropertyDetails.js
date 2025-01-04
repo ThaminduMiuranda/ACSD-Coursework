e32633a5b09334ea5cd4d3a0f20dc2c55fcba04a
ThaminduMiuranda <thaminduthilina@gmail.com>
2025-01-04 16:00:08 +0530

    feat(PropertyDetails): add detailed property view with image gallery
    - **Implemented** image gallery functionality with main image and clickable thumbnails for additional views.
    - **Created** corresponding styles in `PropertyDetails.css` to ensure responsive design and enhanced user experience.
    - **Updated** `properties.json` with new property entries, including detailed descriptions and additional image URLs.
    - **Installed** necessary dependencies for image handling and state management in `package.json`.
    - **Ensured** consistency across styles by updating `index.css` and `SearchBar.css` for unified theming.

--- estate_agent_client-side_web_application/src/Pages/PropertyDetails/PropertyDetails.js
+++ estate_agent_client-side_web_application/src/Pages/PropertyDetails/PropertyDetails.js
@@ -1,9 +1,13 @@
 import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { Link } from "react-router-dom";
+
 import Navbar from "../../Components/NavbarComponent/Navbar";
 import "react-tabs/style/react-tabs.css";
 import "./PropertyDetails.css";
+import { MdBed, MdLocationPin } from "react-icons/md";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 function PropertyDetails() {
   const { id } = useParams(); // matches 'properties/:id.html'
@@ -43,6 +47,20 @@ function PropertyDetails() {
     };
   }, [property]);
 
+  useEffect(() => {
+    const adjustMapSize = () => {
+      const mapElement = mapRef.current;
+      if (mapElement) {
+        mapElement.style.height = window.innerWidth < 640 ? "300px" : "450px";
+      }
+    };
+
+    window.addEventListener("resize", adjustMapSize);
+    adjustMapSize(); // Initial adjustment
+
+    return () => window.removeEventListener("resize", adjustMapSize);
+  }, []);
+
   const images = {
     mainimage: `/img/${id}.jpg`,
     livingroom: `/img/${id}/livingroom.jpg`,
@@ -65,95 +83,133 @@ function PropertyDetails() {
   return (
     <div className="property-page">
       <Navbar />
-      {property && (
-        <>
-          <h1>{property.type}</h1>
-          <div className="image-gallery">
-            <div
-              className="mainimage"
-              style={{
-                backgroundImage: `url(${images[activeImage]})`,
-              }}
-            />
-            <div className="side-panel">
-              {Object.keys(images).map((key) => (
+      <main className="property-main">
+        {property && (
+          <div className="details-wrapper">
+            <div className="sub-details-wrapper">
+              <div className="image-gallery">
                 <div
-                  key={key}
-                  className={`side-image ${
-                    activeImage === key ? "active" : ""
-                  }`}
+                  className="mainimage"
                   style={{
-                    backgroundImage: `url(${images[key]})`,
+                    backgroundImage: `url(${images[activeImage]})`,
                   }}
-                  onClick={() => handleImageClick(key)}
                 />
-              ))}
+                <div className="side-panel">
+                  {Object.keys(images).map((key) => (
+                    <div
+                      key={key}
+                      className={`side-image ${
+                        activeImage === key ? "active" : ""
+                      }`}
+                      style={{
+                        backgroundImage: `url(${images[key]})`,
+                      }}
+                      onClick={() => handleImageClick(key)}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className="sub-details">
+                <div className="general">
+                  <h1>{property.type}</h1>
+                  <div className="location-and-bed">
+                    <p className="location-p">
+                      <MdLocationPin className="icon" /> {property.location}
+                    </p>
+                    <p className="bedroom-p">
+                      <MdBed className="icon" /> {property.bedrooms}
+                    </p>
+                  </div>
+                  <div className="day">
+                    <strong>Date added:</strong>
+                    <span>{property.added.day}</span>
+                    <span>{property.added.month}</span>
+                    <span>{property.added.year}</span>
+                  </div>
+                </div>
+                <p className="tenure-p">
+                  <span>Tenure:</span> {property.tenure}
+                </p>
+
+                <div className="price-details">
+                  <span className="price-type">
+                    {property.type === "House" ? "Price:" : "Rent:"}
+                  </span>
+                  <span className="price">
+                    £{property.price}
+                    <span>{property.type === "House" ? "" : "/ night"}</span>
+                  </span>
+                </div>
+                <Link className="buy-button" to={""}>
+                  {property.type === "House" ? "Buy" : "Rent"}
+                </Link>
+              </div>
             </div>
+            <Tabs forceRenderTabPanel>
+              <TabList>
+                <Tab>Description</Tab>
+                <Tab>Floor Plan</Tab>
+                <Tab>Map</Tab>
+              </TabList>
+              <TabPanel>
+                <div className="description-div">
+                  <span>Property Description</span>
+                  <p className="property-description">{property.description}</p>
+                </div>
+              </TabPanel>
+              <TabPanel>
+                <div className="floor-plan-container">
+                  <div className="topic">
+                    <span>Floor Plan of the </span>
+                    <span>scroll to zoom in</span>
+                  </div>
+                  <div>
+                    <TransformWrapper
+                      initialScale={1}
+                      initialPositionX={0}
+                      initialPositionY={0}
+                      wheel={{ step: 0.1 }}
+                      pinch={{ step: 5 }}
+                    >
+                      <TransformComponent>
+                        <img
+                          className="floor-plan"
+                          src={floorPlan}
+                          alt="Floor Plan"
+                        />
+                      </TransformComponent>
+                    </TransformWrapper>
+                  </div>
+                </div>
+              </TabPanel>
+              <TabPanel>
+                {/* <iframe
+                  ref={mapRef}
+                  id="google-map"
+                  title="google-map"
+                  src={`https://www.google.com/maps?q=${encodeURIComponent(
+                    property.location
+                  )}&output=embed`}
+                  width="600"
+                  height="450"
+                  style={{ border: 0 }}
+                  allowFullScreen=""
+                  loading="lazy"
+                /> */}
+                <iframe
+                  ref={mapRef}
+                  id="google-map"
+                  className="map"
+                  title="google-map"
+                  style={{ border: 0 }}
+                  allowFullScreen=""
+                  loading="lazy"
+                />
+              </TabPanel>
+            </Tabs>
           </div>
-          <Tabs forceRenderTabPanel>
-            <TabList>
-              <Tab>Description</Tab>
-              <Tab>Floor Plan</Tab>
-              <Tab>Map</Tab>
-            </TabList>
-            <TabPanel>
-              <p>{property.description}</p>
-              <p>
-                <strong>Location:</strong> {property.location}
-              </p>
-              <p>
-                <strong>Price:</strong> £{property.price}
-              </p>
-              <p>
-                <strong>Bedrooms:</strong> {property.bedrooms}
-              </p>
-              <p>
-                <strong>Tenure:</strong> {property.tenure}
-              </p>
-            </TabPanel>
-            <TabPanel>
-              <img src={floorPlan} alt="Floor Plan" />
-            </TabPanel>
-            <TabPanel>
-              {/* <iframe
-                ref={mapRef}
-                id="google-map"
-                title="google-map"
-                src={`https://www.google.com/maps?q=${encodeURIComponent(
-                  property.location
-                )}&output=embed`}
-                width="600"
-                height="450"
-                style={{ border: 0 }}
-                allowFullScreen=""
-                loading="lazy"
-              /> */}
-              <iframe
-                ref={mapRef}
-                id="google-map"
-                title="google-map"
-                width="600"
-                height="450"
-                style={{ border: 0 }}
-                allowFullScreen=""
-                loading="lazy"
-              />
-            </TabPanel>
-          </Tabs>
-        </>
-      )}
-      {/* <p>
-        <strong>Location:</strong> {property.location}
-      </p>
-      <p>
-        <strong>Price:</strong> £{property.price}
-      </p>
-      <p>
-        <strong>Bedrooms:</strong> {property.bedrooms}
-      </p>
-      <p>
-        <strong>Tenure:</strong> {property.tenure}
-      </p> */}
+        )}
+      </main>
     </div>
   );
 }
